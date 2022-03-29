2b4dcbeae9754b7becfa4fc34ec2169d4ad1df4d
MarcelaBrjs <ada_wit61@hackademy.mx>
2022-03-28 21:50:01 -0600

    Tarea completada.

---
+++ javascript1.js
@@ -0,0 +1,11 @@
+// JAVASCRIPT 1
+
+// Instrucciones: Crea una función que pueda sumar 2 números enteros.
+
+function suma(x, y) {
+    return x + y;
+}
+
+let x = 2;
+let y = 3;
+console.log("El resultado de la suma es: " + suma(x, y));
